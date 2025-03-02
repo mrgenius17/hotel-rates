aa323042ec91a13a6efbc48570c258a49070fac5
Darshan Thakkar <darshanthakkar716@gmail.com>
2025-03-01 21:45:34 -0500

    Initial Commit

---
+++ app.py
@@ -0,0 +1,118 @@
+# rate price example: https://data.xotelo.com/api/rates?hotel_key=g50207-d101729&chk_in=2025-11-25&chk_out=2025-11-26&currency=USD
+# hotel list example: https://data.xotelo.com/api/list?location_key=g50207&offset=0&limit=30&sort=best_value
+
+import requests
+from datetime import date, timedelta
+import json
+from openpyxl import Workbook, load_workbook
+from openpyxl.styles import PatternFill
+
+cleveland_area_code = "g50207"
+hotel_keys = {
+    "Comfort Inn": "d101729",
+    "Holiday Inn": "d122215",
+    "Hotel Indigo": "d122210",
+    "Drury Plaza": "d9862281",
+    "Residence Inn": "d122181",
+    "DoubleTree by Hilton": "d1372556",
+    "Hilton Garden": "d240589",
+    "Hampton Inn": "d111524",
+    "The Westin": "d95215",
+    "Crowne Plaza": "d95218",
+    "Marriott Key Tower": "d95183",
+    "Aloft": "d4375420"
+}
+base_url = f"https://data.xotelo.com/api/rates?currency=USD"
+today_date = date.today()
+wb = Workbook()
+# wb = load_workbook(f"rates-{today_date}.xlsx")
+sheet = wb.active
+red_fill = PatternFill(start_color="FF6961", end_color="FF6961", fill_type="solid")
+yellow_fill = PatternFill(start_color="FFD700", end_color="FFD700", fill_type="solid")
+green_fill = PatternFill(start_color="77DD77", end_color="77DD77", fill_type="solid")
+start = 0
+end = 2
+
+
+def main():
+
+    write_header(sheet)
+
+    for i in range(start, end):
+        date = today_date + timedelta(days=i)
+        formatted_date = date.strftime("%m/%d")
+
+        sheet.cell(row=2+i, column=1, value=formatted_date)
+
+        for j, hotel_key in enumerate(hotel_keys.keys(), start=2):
+            rate = get_rate(hotel_key, date)
+            sheet.cell(row=2+i, column=j, value=rate)
+
+    color_rates(sheet)
+
+    wb.save(f"rates-{today_date}.xlsx")
+    
+def get_rate(hotel_key, date):
+    url = f"{base_url}&hotel_key={cleveland_area_code}-{hotel_keys[hotel_key]}&chk_in={date}&chk_out={date + timedelta(days=1)}"
+
+    response = requests.get(url)
+    if not response:
+        print(f"Error in fetching rate for hotel: {hotel_key} for date: {today_date}")
+        return None
+    
+    data = json.loads(response.text)
+
+    if "error" in data and data["error"] is not None:
+        print(f"Error {data["error"]["status_code"]}: {data["error"]["message"]}")
+        return None
+
+    if "result" not in data or data["result"] is None:
+        print("Error: Missing 'result' in response.")
+        return None
+
+    rates = data["result"]["rates"]
+    if not rates:
+        print(f"No rates found for hotel: {hotel_key} for date: {today_date}")
+        return None
+    
+    return rates[0]["rate"]
+
+def write_header(sheet):
+    sheet.cell( row=1, column=1 , value="Date")
+    for i, hotel_name in enumerate(list(hotel_keys.keys()), start=2):
+        sheet.cell(row=1, column=i, value=hotel_name)
+
+def color_rates(sheet):
+    for i in range(2, end+2):
+        sheet.cell(row=i, column=2).fill = green_fill
+        if sheet.cell(row=i, column=2).value is None:
+            continue
+        comfortinn_rate = sheet.cell(row=i, column=2).value
+
+        three_lowest_rates = []
+
+        for j in range(3, len(hotel_keys.keys()) + 2):
+            if sheet.cell(row=i, column=j).value is None:
+                continue
+            rate = sheet.cell(row=i, column=j).value
+            cell = sheet.cell(row=i, column=j)
+            
+            if rate < comfortinn_rate:
+                sheet.cell(row=i, column=j).fill = red_fill
+                continue
+            
+            if len(three_lowest_rates)<3:
+                three_lowest_rates.append(cell)
+                three_lowest_rates.sort(key=lambda x: x.value)
+            elif rate < three_lowest_rates[2].value:
+                three_lowest_rates[2] = cell
+                three_lowest_rates.sort(key=lambda x: x.value)
+
+
+        for cell in three_lowest_rates:
+            if cell is not None:
+                cell.fill = yellow_fill        
+
+
+if __name__ == "__main__":
+    main()
